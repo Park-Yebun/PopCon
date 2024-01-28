4543614a5bd7491a951ac888b49e1f762815819f
dbtywi <dbtywi@naver.com>
2024-01-28 14:14:53 +0900

    [Feat][예분] 전체검색페이지 구조 #S10P12C211-278

---
+++ FE/src/views/SearchView.vue
@@ -0,0 +1,101 @@
+<template>
+    <div>
+        <div title="날짜 옵션">
+          <h4>날짜</h4>
+            <div title="button-group">
+                <span>오늘</span>
+                <span>+7일</span>
+                <span>+2주</span>
+                <span>직접선택</span>
+            </div>
+        </div>
+        <div title="지역 옵션">
+            <h4>지역</h4>
+            <div title="button-group">
+                <span>전체</span>
+                <span>서울특별시</span>
+                <span>경기도</span>
+                <span>광주광역시</span>
+                <span>인천광역시</span>
+                <span>강원도</span>
+                <span>부산광역시</span>
+                <span>대전광역시</span>
+            </div>
+        </div>
+        <div title="카테고리 옵션">
+            <h4>카테고리</h4>
+            <div title="button-group">
+                <span>
+                    <img src="#" alt="#">
+                    <span>패션/뷰티</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>식음료</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>콘텐츠</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>취미/여가</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>금융</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>연예</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>가전/디지털</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>리빙</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>게임</span>
+                </span>
+                <span>
+                    <img src="#" alt="#">
+                    <span>캐릭터</span>
+                </span>
+            </div>
+        </div>
+        <div title="운영상태 옵션">
+            <h4>운영상태</h4>
+            <div title="button-group">
+                <span>진행중</span>
+                <span>오픈 예정</span>
+                <span>종료</span>
+            </div>
+        </div>
+        <div title="팝업스토어 정렬">
+            <div>
+                <select name="" id="">
+                    <option value="">최신순</option>
+                    <option value="">리뷰 많은순</option>
+                    <option value="">좋아요 순</option>
+                    <option value="">조회수</option>
+                    <option value="">마감순</option>
+                </select>
+            </div>
+            <div title="popup-group">
+                <img src="#" alt="정렬된 팝업 목록">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
