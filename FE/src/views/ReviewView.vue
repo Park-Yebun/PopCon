09d53a4453b39514d0c9ce26882b06f84985ec8b
dbtywi <dbtywi@naver.com>
2024-01-29 10:25:31 +0900

    [Feat][예분] 리뷰페이지 구조 #S10P12C211-325

---
+++ FE/src/views/ReviewView.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div title="뒤로가기 버튼">
+            <a href="#">X</a>
+        </div>
+        <div title="해시태그 선택하기">
+            <div title="안내 멘트">
+                <h2>어떤 점이 좋았나요?</h2>
+                <h4>이 가게에 어울리는 키워드를 골라주세요.</h4>
+            </div>
+            <div title="선택 옵션">
+                <div title="상품">
+                    <h4>상품</h4>
+                    <ul>
+                        <li>품질이 좋아요</li>
+                        <li>가성비가 좋아요</li>
+                        <li>종류가 다양해요</li>
+                        <li>독특한 디자인이 많아요</li>
+                        <li>트렌디해요</li>
+                        <li>신상품이 많아요</li>
+                    </ul>
+                </div>
+                <div title="서비스/기타">
+                    <h4>서비스/기타</h4>
+                    <ul>
+                        <li>친절해요</li>
+                        <li>매장이 넓어요</li>
+                        <li>주차하기 편해요</li>
+                        <li>컨셉이 독특해요</li>
+                        <li>시설이 깔끔해요</li>
+                        <li>선물하기 좋아요</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div title="이미지 첨부">
+            <h3>이미지를 추가해 주세요</h3>
+            <input type="file" name="" id="">
+        </div>
+        <form title="리뷰 작성">
+            <div title="별점 레이팅">
+                <fieldset>
+                    <input type="radio" value="1">
+                    <input type="radio" value="2">
+                    <input type="radio" value="3">
+                    <input type="radio" value="4">
+                    <input type="radio" value="5">
+                </fieldset>   
+            </div>
+            <div>
+                <textarea name="" id="" cols="47" rows="10" placeholder="리뷰를 작성해주세요&#13;&#10;&#13;&#10;    
+                리뷰를 보는 사용자와 사업자에게 상처가 되는욕설, 비방, 명예훼손성 표현은 사용하지 말아주세요."></textarea>
+            </div>
+        </form>
+        <div title="하단 버튼">
+            <a href="#">이전</a>
+            <a href="#">등록</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
