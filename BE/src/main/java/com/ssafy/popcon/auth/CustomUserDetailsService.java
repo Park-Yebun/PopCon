fe3ea18d19f3e5e63284cebe659f7d291b832c33
lsy5490 <lsy5490@naver.com>
2024-01-29 14:07:02 +0900

    [Feat][승연] 회원 탈퇴 #S10P12C211-312

--- BE/src/main/java/com/ssafy/popcon/auth/CustomUserDetailsService.java
+++ BE/src/main/java/com/ssafy/popcon/auth/CustomUserDetailsService.java
@@ -21,6 +21,9 @@ public class CustomUserDetailsService implements UserDetailsService {
         UserDto userDto= null;
         try {
             userDto = userMapper.findUserById(userId);
+            if (userDto == null) {
+                throw new UsernameNotFoundException("아이디 : "+userId+"에 해당하는 사용자가 없습니다");
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
