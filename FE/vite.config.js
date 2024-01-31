37ba5370753df668f53b42120b8746d2e33ed798
Muncheolhwan <muncheolhwan@naver.com>
2024-01-31 17:45:21 +0900

    [feat][철환]PWA구현

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -2,15 +2,84 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  workboxOptions: {
+    include: [/^index\.html$/, /\.css$/, /\.js$/, /^manifest\.json$/, /\.png$/],
+    exclude: []
+  },
+  publicPath: '/',
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      filename: 'manifest.json',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        "name": "POP-CON",
+        "short_name": "POP-CON",
+        "icons": [
+          {
+            "src": "/img/icons/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/img/icons/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/img/icons/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/img/icons/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#ffffff"
+      },
+      workbox: {
+        runtimeCaching: [{
+                urlPattern: /\.png$/,
+                handler: 'CacheFirst',
+                options: {
+                    cacheName: 'png-cache',
+                    expiration: {
+                        maxEntries: 10, // 총 파일 10개까지 캐시
+                        maxAgeSeconds: 60 * 60 * 24 * 365, // 1년 캐시 
+                    }
+                }
+            },
+            {
+                urlPattern: /\.json$/,
+                handler: 'StaleWhileRevalidate',
+                options: {
+                    cacheName: 'json-cache',
+                    cacheableResponse: {
+                        statuses: [200]
+                    }
+                },
+            }
+        ],
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
