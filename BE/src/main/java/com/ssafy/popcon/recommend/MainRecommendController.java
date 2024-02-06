21a289c2f057a31c1fe2fe98b078b0d7d9b1095d
lsy5490 <lsy5490@naver.com>
2024-02-06 13:45:15 +0900

    [Feat][승연] 팝비티아이 결과 조회 기능

---
+++ BE/src/main/java/com/ssafy/popcon/recommend/MainRecommendController.java
@@ -0,0 +1,12 @@
+package com.ssafy.popcon.recommend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recommends")
+public class MainRecommendController {
+    // 추천 페이지 최초 진입 컨트롤러
+}
