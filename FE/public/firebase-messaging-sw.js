6ef3f48e280208f173fe50aa9a878844ff39c2fa
dbtywi <dbtywi@naver.com>
2024-02-14 16:51:50 +0900

    [Feat][예분] 푸쉬알람 수정

--- FE/public/firebase-messaging-sw.js
+++ FE/public/firebase-messaging-sw.js
@@ -1,70 +1,70 @@
-// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// firebase.initializeApp({
-//     apiKey: "AIzaSyDvNw-1CiYp-H2q_RTsM_uM778SsrWj2ZM",
-//     authDomain: "ssafypopcon.firebaseapp.com",
-//     projectId: "ssafypopcon",
-//     storageBucket: "ssafypopcon.appspot.com",
-//     messagingSenderId: "543956021805",
-//     appId: "1:543956021805:web:07b2670024d09e5ec9fea4",
-//     measurementId: "G-5QZKSRXRD2"
-// });
+firebase.initializeApp({
+    apiKey: "AIzaSyDvNw-1CiYp-H2q_RTsM_uM778SsrWj2ZM",
+    authDomain: "ssafypopcon.firebaseapp.com",
+    projectId: "ssafypopcon",
+    storageBucket: "ssafypopcon.appspot.com",
+    messagingSenderId: "543956021805",
+    appId: "1:543956021805:web:07b2670024d09e5ec9fea4",
+    measurementId: "G-5QZKSRXRD2"
+});
 
-// const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-// messaging.onBackgroundMessage((payload) => {
-//     console.log(
-//         '[firebase-messaging-sw.js] Received background message ',
-//         payload
-//     );
-//     // Customize notification here
-//     const notificationTitle = payload.notification.title;
-//     const notificationOptions = {
-//         body: playload.notification.body,
-//         icon: '/icon.png'
-//     };
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+        '[firebase-messaging-sw.js] Received background message ',
+        payload
+    );
+    // Customize notification here
+    // const notificationTitle = payload.notification.title;
+    // const notificationOptions = {
+    //     body: payload.notification.body,
+    //     icon: '/icon.png'
+    // };
 
-//     self.registration.showNotification(notificationTitle, notificationOptions);
-// });
+    // self.registration.showNotification(notificationTitle, notificationOptions);
+});
 
-// // 추가코드
-// self.addEventListener("install", function (e) {
-//     self.skipWaiting();
-// });
+// 추가코드
+self.addEventListener("install", function (e) {
+    self.skipWaiting();
+});
   
-// self.addEventListener("activate", function (e) {
-//     console.log("fcm sw activate..");
-// });
+self.addEventListener("activate", function (e) {
+    console.log("fcm sw activate..");
+});
 
-// self.addEventListener("notificationclose", function (event) {
-//     console.log("Notification closed");
-//     // Add your custom handling for notification close event here
-// });
+self.addEventListener("notificationclose", function (event) {
+    console.log("Notification closed");
+    // Add your custom handling for notification close event here
+});
 
 
-// self.addEventListener('notificationclick', function(event) {
-//     event.notification.close(); // 알림 닫기
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close(); // 알림 닫기
   
-//     // 알림 클릭 후 이동할 URL 설정
-//     var urlToOpen = 'https://example.com';
+    // 알림 클릭 후 이동할 URL 설정
+    var urlToOpen = 'https://i10c211.p.ssafy.io';
   
-//     // 클라이언트 창이 열려 있으면 포커스를 해당 창으로 이동
-//     event.waitUntil(
-//       clients.matchAll({
-//         type: 'window'
-//       })
-//       .then(function(clientList) {
-//         for (var i = 0; i < clientList.length; i++) {
-//           var client = clientList[i];
-//           if (client.url === urlToOpen && 'focus' in client) {
-//             return client.focus();
-//           }
-//         }
-//         // 클라이언트 창이 없으면 새 창을 열고 해당 URL로 이동
-//         if (clients.openWindow) {
-//           return clients.openWindow(urlToOpen);
-//         }
-//       })
-//     );
-//   });
+    // 클라이언트 창이 열려 있으면 포커스를 해당 창으로 이동
+    event.waitUntil(
+      clients.matchAll({
+        type: 'window'
+      })
+      .then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url === urlToOpen && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        // 클라이언트 창이 없으면 새 창을 열고 해당 URL로 이동
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen);
+        }
+      })
+    );
+  });
--- FE/src/stores/counter.js
+++ FE/src/stores/counter.js
@@ -23,51 +23,51 @@ export const useCounterStore = defineStore('counter', () => {
 
   
   // 파이어베이스 생성 토큰 받아오기
-  onMounted(()=>{
-    navigator.serviceWorker
-  .register("firebase-messaging-sw.js")
-  .then(function (registration) {
-    console.log("Service worker successfully registered.");
-    return registration;
-  })
-  .catch(function (err) {
-    console.error("Unable to register service worker.", err);
-  });
+  // onMounted(()=>{
+  //   navigator.serviceWorker
+  // .register("firebase-messaging-sw.js")
+  // .then(function (registration) {
+  //   console.log("Service worker successfully registered.");
+  //   return registration;
+  // })
+  // .catch(function (err) {
+  //   console.error("Unable to register service worker.", err);
+  // });
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDvNw-1CiYp-H2q_RTsM_uM778SsrWj2ZM",
-    authDomain: "ssafypopcon.firebaseapp.com",
-    projectId: "ssafypopcon",
-    storageBucket: "ssafypopcon.appspot.com",
-    messagingSenderId: "543956021805",
-    appId: "1:543956021805:web:07b2670024d09e5ec9fea4",
-    measurementId: "G-5QZKSRXRD2"
-  };
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyDvNw-1CiYp-H2q_RTsM_uM778SsrWj2ZM",
+  //   authDomain: "ssafypopcon.firebaseapp.com",
+  //   projectId: "ssafypopcon",
+  //   storageBucket: "ssafypopcon.appspot.com",
+  //   messagingSenderId: "543956021805",
+  //   appId: "1:543956021805:web:07b2670024d09e5ec9fea4",
+  //   measurementId: "G-5QZKSRXRD2"
+  // };
   
   
-  const app1 = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app1);
+  // const app1 = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app1);
   
-  const messaging = getMessaging();
+  // const messaging = getMessaging();
 
-  getToken(messaging, { vapidKey: 'BJK9lVeFIvJ5u3jvtWKGabTSNOqbX69MT2m2gbl110ZDyvUFsvpkKKHRKZRd4wEdjopFz_NxuGgfZoET1kTeqGs' }).then((currentToken) => {
-      if (currentToken) {
-          // Send the token to your server and update the UI if necessary
-          fcmToken.value = currentToken
-          console.log(currentToken)
-          // ...
-      } else {
-          // Show permission request UI
-          console.log('No registration token available. Request permission to generate one.');
-          // ...
-      }
+  // getToken(messaging, { vapidKey: 'BJK9lVeFIvJ5u3jvtWKGabTSNOqbX69MT2m2gbl110ZDyvUFsvpkKKHRKZRd4wEdjopFz_NxuGgfZoET1kTeqGs' }).then((currentToken) => {
+  //     if (currentToken) {
+  //         // Send the token to your server and update the UI if necessary
+  //         fcmToken.value = currentToken
+  //         console.log(currentToken)
+  //         // ...
+  //     } else {
+  //         // Show permission request UI
+  //         console.log('No registration token available. Request permission to generate one.');
+  //         // ...
+  //     }
   
-  }).catch((err) => {
-      console.log('An error occurred while retrieving token. ', err);
-      // ...
-  });
+  // }).catch((err) => {
+  //     console.log('An error occurred while retrieving token. ', err);
+  //     // ...
+  // });
 
-  })
+  // })
 
 
   return { nowURL, fcmToken  }
